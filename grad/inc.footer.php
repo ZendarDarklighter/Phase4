76e5f770318e846c3a1cb4966ad27faaf9803b1b
carl <notbob668@gmail.com>
2017-11-01 22:10:34 -0500

    inc files for header and footer and the css file for them

---
+++ grad/inc.footer.php
@@ -0,0 +1,24 @@
+<!-- 
+    
+    File: inc.footer.php
+    Author: Carl Grissom
+    Team: Bugs
+    Date: 10/31/2017
+    CS-321 pahse 4
+
+    Description: html footer file for Biological Sciences site
+                 
+-->
+
+
+</main>
+
+    <footer>
+        <img src="../images/logo-edwardsville.png" alt="SIUE logo">
+        <div>
+            <?php echo "$copyright_year $copyright_text";?>
+        </div>
+    </footer>
+
+    </body>
+</html>
